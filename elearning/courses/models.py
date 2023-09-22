19f98ed8aaa2cd4640d3077b95442904996f3f3d
Jose Mosquera <jojumofu@gmail.com>
2023-09-22 14:00:47 -0500

    feat: add ordering to model and content

--- elearning/courses/models.py
+++ elearning/courses/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
         ordering = ["title"]
@@ -17,10 +18,10 @@ class Subject(models.Model):
 
 class Course(models.Model):
     owner = models.ForeignKey(
-        User, related_name="course_created", on_delete=models.CASCADE
+        User, related_name="courses_created", on_delete=models.CASCADE
     )
     subject = models.ForeignKey(
-        Subject, related_name="course", on_delete=models.CASCADE
+        Subject, related_name="courses", on_delete=models.CASCADE
     )
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
@@ -38,6 +39,10 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name="modules", on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=["course"])
+
+    class Meta:
+        ordering = ["order"]
 
     def __str__(self):
         return f"{self.order}. {self.title}"
@@ -45,14 +50,19 @@ class Module(models.Model):
 
 class Content(models.Model):
     module = models.ForeignKey(
-        Module,
-        related_name="contents",
+        Module, related_name="contents", on_delete=models.CASCADE
+    )
+    content_type = models.ForeignKey(
+        ContentType,
         on_delete=models.CASCADE,
         limit_choices_to={"model__in": ("text", "video", "image", "file")},
     )
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey("content_type", "object_id")
+    order = OrderField(blank=True, for_fields=["module"])
+
+    class Meta:
+        ordering = ["order"]
 
 
 class ItemBase(models.Model):
@@ -60,8 +70,8 @@ class ItemBase(models.Model):
         User, related_name="%(class)s_related", on_delete=models.CASCADE
     )
     title = models.CharField(max_length=250)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
