afe8dbe4a6a23641b231f5be9fca6c2d99edce4e
Jose Mosquera <jojumofu@gmail.com>
2023-09-25 07:22:12 -0500

    feat: create class-based view for cms

--- elearning/courses/views.py
+++ elearning/courses/views.py
@@ -1,3 +1,14 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Course
 
-# Create your views here.
+
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = "manage_course_list.html"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        query_set = super().get_queryset()
+        return query_set.filter(owner=self.request.user)
