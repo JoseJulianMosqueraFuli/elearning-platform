0ce2c2f1267577b377b320be7a90c273e03878fb
Jose Mosquera <jojumofu@gmail.com>
2023-09-26 15:15:28 -0500

    feat: create mixins

--- elearning/courses/views.py
+++ elearning/courses/views.py
@@ -1,7 +1,10 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Course
 
 
@@ -12,3 +15,37 @@ class ManageCourseListView(ListView):
     def get_queryset(self) -> QuerySet[Any]:
         query_set = super().get_queryset()
         return query_set.filter(owner=self.request.user)
+
+
+class OwnerMixin:
+    def get_queryset(self):
+        query_set = super.get_queryset()
+        return query_set.filter(owner=self.request.user)
+
+
+class OwnerEditMixin:
+    def from_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+    field = ["subject", "title", "slug", "overview"]
+    success_url = reverse_lazy("manage_course_list")
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    pass
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    pass
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    pass
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    pass
