a17228838d31c6e888dc0e379fe7e41340d91038
Jose Mosquera <jojumofu@gmail.com>
2023-09-29 07:05:05 -0500

    fix: typos in views

--- elearning/courses/views.py
+++ elearning/courses/views.py
@@ -1,11 +1,8 @@
-from typing import Any
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-
 from .models import Course
 
 
@@ -13,26 +10,26 @@ class ManageCourseListView(ListView):
     model = Course
     template_name = "courses/manage/course/list.html"
 
-    def get_queryset(self) -> QuerySet[Any]:
-        query_set = super().get_queryset()
-        return query_set.filter(owner=self.request.user)
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
 
 
 class OwnerMixin:
     def get_queryset(self):
-        query_set = super.get_queryset()
-        return query_set.filter(owner=self.request.user)
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
 
 
 class OwnerEditMixin:
-    def from_valid(self, form):
+    def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
 
 
 class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
     model = Course
-    field = ["subject", "title", "slug", "overview"]
+    fields = ["subject", "title", "slug", "overview"]
     success_url = reverse_lazy("manage_course_list")
 
 
