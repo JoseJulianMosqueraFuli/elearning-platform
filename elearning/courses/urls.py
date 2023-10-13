de3c08cb0d536c7727bb34eb11d3849bd580ac64
Jose Mosquera <jojumofu@gmail.com>
2023-10-13 17:48:57 -0500

    feat: add view to display courses

--- elearning/courses/urls.py
+++ elearning/courses/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+from courses.views import CourseListView
+
 urlpatterns = [
     path("mine/", views.ManageCourseListView.as_view(), name="manage_course_list"),
     path("create/", views.CourseCreateView.as_view(), name="course_create"),
@@ -33,4 +35,5 @@ urlpatterns = [
     ),
     path("module/order/", views.ModuleOrderView.as_view(), name="module_order"),
     path("content/order/", views.ContentOrderView.as_view(), name="content_order"),
+    path("", CourseListView.as_view(), name="course_list"),
 ]
