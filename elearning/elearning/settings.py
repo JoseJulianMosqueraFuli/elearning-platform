6e9a8299532e1757f94c1adbc2cb7b78bb286a26
Jose Mosquera <jojumofu@gmail.com>
2023-11-08 18:11:44 -0500

    feat: accessing the course content

--- elearning/elearning/settings.py
+++ elearning/elearning/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from django.urls import reverse_lazy
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +127,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Config Sever Media Files
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
+LOGIN_REDIRECT_URL = reverse_lazy("student_course_list")
--- elearning/students/views.py
+++ elearning/students/views.py
@@ -1,10 +1,16 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.views.generic.list import ListView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.views.generic.edit import FormView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .forms import CourseEnrollForm
+from courses.models import Course
 
 
 class StudentRegistrationView(CreateView):
@@ -34,3 +40,33 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
 
     def get_access_url(self):
         return reverse_lazy("student_course_detail", args=[self.course.id])
+
+
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = "students/course/list.html"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+
+class StudentCourseDetailView(DetailView):
+    model = Course
+    template_name = "students/course/detail.html"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # get course object
+        course = self.get_object()
+        if "module_id" in self.kwargs:
+            # get current module
+            context["module"] = course.modules.get(id=self.kwargs["module_id"])
+        else:
+            # get first module
+            context["module"] = course.modules.all()[0]
+        return context
