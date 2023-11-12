3853ecddf1ac9cc297bbad34a3da13ee426c0ea0
Jose Mosquera <jojumofu@gmail.com>
2023-11-11 19:11:23 -0500

    chore: add debug_toolbar to django settings

--- elearning/elearning/settings.py
+++ elearning/elearning/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "embed_video",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
