4000a8ab2092fb5382d4c1dd80107999049854c7
Jose Mosquera <jojumofu@gmail.com>
2023-11-18 17:23:09 -0500

    chore: create course serializer

--- elearning/courses/api/serializers.py
+++ elearning/courses/api/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
-from courses.models import Subject
+from courses.models import Subject, Course
 
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ["id", "title", "slug"]
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = [
+            "id",
+            "subject",
+            "title",
+            "slug",
+            "overview",
+            "created_at",
+            "owner",
+            "modules",
+        ]
