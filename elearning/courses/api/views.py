d0b766ffda54795a602e2d5ec8444ee2a4bb740c
Jose Mosquera <jojumofu@gmail.com>
2023-11-28 20:05:39 -0500

    feat: add permission for user authenticated

--- elearning/courses/api/views.py
+++ elearning/courses/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from courses.models import Subject, Course
 from courses.api.serializers import SubjectSerializer
@@ -21,6 +22,7 @@ class CourseEnrollView(APIView):
     authentication_classes = [
         BasicAuthentication,
     ]
+    permission_classes = [IsAuthenticated]
 
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
