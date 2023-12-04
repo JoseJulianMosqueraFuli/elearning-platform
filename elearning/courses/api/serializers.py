bf6ac8ea8c3e2df179c5893e40dd6cafed9ec8c8
Jose Mosquera <jojumofu@gmail.com>
2023-12-04 11:01:10 -0500

    feat: add content serializer with and without content

--- elearning/courses/api/serializers.py
+++ elearning/courses/api/serializers.py
@@ -47,3 +47,28 @@ class ContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Content
         fields = ["order", "item"]
+
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ["order", "title", "description", "contents"]
+
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = [
+            "id",
+            "subject",
+            "title",
+            "slug",
+            "overview",
+            "created",
+            "owner",
+            "modules",
+        ]
