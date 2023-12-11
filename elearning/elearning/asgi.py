d575aa1c9f5f0d1cdcb20ff5b2e649c9276e3f05
Jose Mosquera <jojumofu@gmail.com>
2023-12-11 09:49:44 -0500

    feat: add router to asgi

--- elearning/elearning/asgi.py
+++ elearning/elearning/asgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+import chat.routing as routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elearning.settings")
 
@@ -18,5 +21,6 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
     }
 )
